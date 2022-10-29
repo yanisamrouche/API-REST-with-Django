9275b8733f8f9e461a6f0ae74cf0e90f3b22efe6
yanis <yann.amrouche@gmail.com>
2022-10-29 23:56:12 +0200

    api with django rest framework v2 GET & POST

--- users/drf_api/urls.py
+++ users/drf_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import UserViewSet
-
+#from .views import UserViewSet
+from .views import UserListApiView
 router = routers.DefaultRouter()
-router.register(r'users',UserViewSet)
+#router.register(r'users',UserViewSet)
 
 urlpatterns = [
-    path('',include(router.urls))
+    #path('',include(router.urls))
+    path('', UserListApiView.as_view())
 ]
--- users/drf_api/views.py
+++ users/drf_api/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+class UserListApiView(APIView):
+    #1.get all users
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    #2.create users
+    def post(self, request, *args, **kwargs):
+        data = {
+            'first_name': request.data.get('first_name'),
+            'last_name': request.data.get('last_name'),
+            'gender': request.data.get('gender'),
+            'email': request.data.get('email'),
+
+        }
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
